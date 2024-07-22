98d9c94c68515cfe206db77304e06c74abbca8c5
ArtemBarkov <artemka.barik@gmail.com>
2024-07-22 23:21:34 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+Course='Python'
+total_tasks='12'
+hours_spent='1.5'
+average_execution_time='0.125 часа '
+print(Course, total_tasks, hours_spent, average_execution_time)
+Course='Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.'
+print(Course)
